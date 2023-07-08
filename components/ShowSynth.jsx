26f758427466e4a0409f0ed579f4263f198a9e48
Ben <b.e.eidson@gmail.com>
2023-07-08 12:44:54 -0400

    FUNCTIONING POSTING of SYNTH VERSION 1

--- components/ShowSynth.jsx
+++ components/ShowSynth.jsx
@@ -1,10 +1,64 @@
 "use client";
 
+import * as Tone from 'tone'
 import { NextReactP5Wrapper } from "@p5-wrapper/next";
 import { Suspense } from "react";
 import { useRef } from "react";
+import {
+  granularOptions,
+  synthStart,
+  synthMove,
+  synthEnd,
+} from "@/lib/synth/soundHelper";
+
+import { drawFunction } from "@/lib/synth/visualHelper";
 
 const ShowSynth = ({ actionsArray }) => {
+  //SOUND///
+  const fmSynth = new Tone.FMSynth();
+  fmSynth.oscillator.type = "sawtooth";
+
+  const randomPulseSynth = new Tone.Synth();
+  randomPulseSynth.oscillator.type = "sine";
+
+  const granularSynth = new Tone.GrainPlayer(granularOptions);
+  const sineSynth = new Tone.Synth();
+
+  const bitCrusher = new Tone.BitCrusher({
+    bits: 8,
+  });
+  const reverb = new Tone.Reverb({
+    decay: 5,
+  });
+  const limiter = new Tone.Limiter();
+  limiter.threshold.value = -50; // Adjust the threshold level as needed
+  const compressor = new Tone.Compressor(-20, 3);
+
+  const reverb1 = new Tone.Reverb({
+    decay: 0.5,
+  });
+
+  const distortion1 = new Tone.Distortion(0.3);
+
+  fmSynth.chain(reverb1, distortion1, Tone.Destination);
+  randomPulseSynth.chain(
+    reverb,
+    bitCrusher,
+    compressor,
+    limiter,
+    Tone.Destination
+  );
+  granularSynth.chain(Tone.Destination);
+  sineSynth.chain(Tone.Destination);
+
+  function logisticMap(x, r) {
+    //randomizer for the pitch of pulseSynth
+    return r * x * (1 - x);
+  }
+  //values for randomizing pulse
+  let value2 = 0.5; // Set the initial value (between 0 and 1)
+  const parameter2 = 3.2; // Set the parameter (can be changed)
+
   const isPlaying = useRef(false); // Use useRef for mutable state
   let p5Instance; // Variable to store the p5 instance
   let recordingStartTime;
@@ -25,7 +79,9 @@ const ShowSynth = ({ actionsArray }) => {
 
     p5.draw = function () {
       if (isPlaying) {
-        let elapsedTime = p5Instance.millis() - recordingStartTime;
+        let elapsedTime = p5.millis() - recordingStartTime;
+        const scaleX = p5.width; //scale to deal with normalized value
+        const scaleY = p5.height;
 
         progress = (elapsedTime / recordingDuration) * p5.width; //calc progress
 
@@ -33,25 +89,36 @@ const ShowSynth = ({ actionsArray }) => {
           let action = mouseActions[playbackIndex];
           if (action.time <= elapsedTime) {
             if (action.event === "dragged") {
-              p5Instance.ellipse(
-                action.x * p5.width,
-                action.y * p5.height,
-                30,
-                30
+              drawFunction(
+                p5,
+                action.x * scaleX,
+                action.y * scaleY,
+                action.prevX * scaleX,
+                action.prevY * scaleY
               );
+              synthMove(p5, fmSynth, randomPulseSynth, granularSynth, sineSynth, action.x * scaleX, action.y * scaleY,)
             }
 
             if (action.event === "click") {
-              p5Instance.ellipse(
-                action.x * p5.width,
-                action.y * p5.height,
-                30,
-                30
+              drawFunction(
+                p5,
+                action.x * scaleX,
+                action.y * scaleY,
+                action.prevX * scaleX,
+                action.prevY * scaleY
               );
+              synthStart(p5, fmSynth, randomPulseSynth, granularSynth, sineSynth, action.x * scaleX, action.y * scaleY)
             }
 
             if (action.event === "release") {
-              p5Instance.ellipse(action.x * p5.width, action.y * p5.height, 30, 30);
+              drawFunction(
+                p5,
+                action.x * scaleX,
+                action.y * scaleY,
+                action.prevX * scaleX,
+                action.prevY * scaleY
+              );
+              synthEnd(p5, fmSynth, randomPulseSynth, granularSynth, sineSynth)
             }
             playbackIndex++;
           } else {
@@ -79,6 +146,9 @@ const ShowSynth = ({ actionsArray }) => {
     p5Instance.background(255);
     playbackIndex = 0;
     console.log("bang");
+
+    //reset synth
+    
   };
 
   return (
@@ -89,7 +159,12 @@ const ShowSynth = ({ actionsArray }) => {
         }
       >
         <NextReactP5Wrapper sketch={sketch} />
-        <button onClick={startPlayback} className=" hover:text-green-500 hover:underline">START</button>
+        <button
+          onClick={startPlayback}
+          className=" hover:text-green-500 hover:underline"
+        >
+          START
+        </button>
       </Suspense>
     </>
   );
@@ -99,6 +174,5 @@ export default ShowSynth;
 
 //add some error logic with Array.isArray(arrayasdfa)
 //NOT STORING PROPER IMAGE END
-
 
 //add a pause button and such
