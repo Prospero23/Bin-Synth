e4719a7b0702155c89f989ae20bf94ab704bb3fb
Ben <b.e.eidson@gmail.com>
2023-08-19 13:56:35 -0700

    no double click auth anymore!

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -10,30 +10,27 @@ export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter()
 
-  const loginWithGoogle = async () => {
+  const loginWithGoogle = () => {
     setIsLoading(true);
+    console.log('bang')
 
     try {
-      await signIn("google");
+      signIn("google", { callbackUrl: '/users/login/loggedIn' })
     } catch (error) {
       //send TOAST notification to user
       toast.error('Error signing in')
     } finally {
       setIsLoading(false);
-      router.push('/users/login/loggedIn')
     }
   };
 
   return (
-    <div className="bg-gray-100 text-black btn hover:bg-sky-500 w-96 my-2 mt-4 flex">
-      <button onClick={loginWithGoogle} className="w-full">
+      <button onClick={loginWithGoogle} className="bg-gray-100 text-black btn hover:bg-sky-500 w-96 my-2 mt-4 flex">
         <div className="flex items-center">
         {isLoading ? <span className="loading loading-spinner loading-sm mr-24"></span>: <FaGoogle className='mr-24' />}
           <span className="">Continue with Google</span>
         </div>
       </button>
-    </div>
-
   );
 }
 
